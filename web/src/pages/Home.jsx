3353fcc78bc41d68ca3822766ebd3ec74779ad27
Danilo Goulart <danilogoulart@outlook.com.br>
2026-07-22 21:22:24 -0300

    Add debug log statement for testing purposes in Home component

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -37,6 +37,8 @@ export default function Home() {
 
   const lastProj = fc && fc.rows.length ? fc.rows[fc.rows.length - 1] : null
 
+  console.log('Teste Daniel');
+
   return (
     <>
       {error && <div className="card"><div className="banner err">{error}</div></div>}
